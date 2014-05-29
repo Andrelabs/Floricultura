7f3e26332712e8ce5a86a404bd402c9cad6774a7
Jefferson Cunha <jefferson.cunha@totalip.com.br>
2014-05-29 15:10:59 -0300

    Commit inicial
    Signed-off-by: Jefferson Cunha <jefferson.cunha@totalip.com.br>

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Lojavirtual::Application.config.secret_key_base = '1023722b99ed703a36e570ff9ba096ed85f22e63db07674e15df4d07c9e18437c43f822f798290c0cd662885ab9e6f5806dd58400546fa30a7a313118b93f2a2'
